568b0c36dd7d9a57565c8786d3924604ba973567
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-07-18 17:04:15 +0900

    selection sort & bubble sort

--- algorithm-tutorial/selectionSort.cpp
+++ algorithm-tutorial/selectionSort.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main() {
+int selectionSort() {
     int i, j, min, index = 0, temp;
     int array[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
-    
     for (i = 0; i < 10; i++) {
         min = 9999;
         for (j = i; j < 10; j++) {
@@ -16,10 +15,8 @@ int main() {
         array[i] = array[index];
         array[index] = temp;
     }
-    
     for (i = 0; i < 10; i++) {
-        printf("\n%d", array[i]);
+        printf("%d\n", array[i]);
     }
-    
     return 0;
 }
